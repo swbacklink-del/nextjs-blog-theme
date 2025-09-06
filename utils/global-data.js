933cb5b281c6d86239ab8b4fc71ad04911d5265b
nurielmos <nuriel.mos88@gmail.com>
2025-09-07 00:21:24 +0700

    4

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'My Blog';
+    : 'Panduan Lengkap Membuat Website Company Profile untuk Pemula';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Dunia Bisnis';
+    : 'Panduan lengkap membuat website company profile untuk pemula. Pelajari langkah mudah mulai dari domain, desain, konten, hingga optimasi SEO agar bisnis terlihat profesional dan terpercaya.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
